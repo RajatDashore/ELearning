a74c7678463bb720dea71d789a3cda21374a6d96
Rajat Dashore <Rajat Dashore@LWS.LOCAL>
2026-06-19 12:20:50 +0530

    L

--- app/src/main/java/com/example/elearning/navigation/views/HomeActivity.kt
+++ app/src/main/java/com/example/elearning/navigation/views/HomeActivity.kt
@@ -2,12 +2,14 @@ package com.example.elearning.navigation.views
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.example.elearning.R
 import com.example.elearning.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private val TAG = "HomeActivity"
     private lateinit var binding: ActivityHomeBinding
     private var index = 0
 
@@ -28,6 +30,49 @@ class HomeActivity : AppCompatActivity() {
             return
         }
         val navController = navHostFragment?.navController
+
+
+        navController?.addOnDestinationChangedListener { controller, destination, bundle ->
+            if (destination.id == R.id.fourthFragment) {
+                binding.btnNext.visibility = View.GONE
+            } else {
+                binding.btnNext.visibility = View.VISIBLE
+            }
+
+
+            if (destination.id == R.id.firstFragment) {
+                binding.btnPre.visibility = View.GONE
+            } else {
+                binding.btnPre.visibility = View.VISIBLE
+            }
+            Log.d(TAG, "onCreate: ")
+
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            when (index) {
+                0 -> {
+                    navController?.navigate(R.id.action_firstFragment_to_thirdFragment)
+                    index = 2
+                }
+
+                1 -> {
+                    navController?.navigate(R.id.action_secondFragment_to_fourthFragment)
+                    index = 3
+                }
+
+                2 -> {
+                    navController?.navigate(R.id.action_thirdFragment_to_firstFragment)
+                    index = 0
+                }
+
+                3 -> {
+                    navController?.navigate(R.id.action_fourthFragment_to_secondFragment)
+                    index = 1
+                }
+
+            }
+        }
 
 
         binding.btnPre.setOnClickListener {
